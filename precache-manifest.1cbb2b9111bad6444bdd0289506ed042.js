72bbb9e72132a4165a97a19eb92114529141f6bc
Ekaterina <ekaterinan112@gmail.com>
2020-10-13 01:44:51 +0300

    Updates

---
+++ precache-manifest.1cbb2b9111bad6444bdd0289506ed042.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6cc2f092c0d499156929e6f2acd247a",
+    "url": "/counter/index.html"
+  },
+  {
+    "revision": "e37418104e9f5d9f87cc",
+    "url": "/counter/static/css/main.f55a7baf.chunk.css"
+  },
+  {
+    "revision": "521045111b4da56b4cc5",
+    "url": "/counter/static/js/2.aa78e3b0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/counter/static/js/2.aa78e3b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e37418104e9f5d9f87cc",
+    "url": "/counter/static/js/main.f9435337.chunk.js"
+  },
+  {
+    "revision": "d2d680e0ce955b1c0fad",
+    "url": "/counter/static/js/runtime-main.dc337d20.js"
+  },
+  {
+    "revision": "ba55fe4d6e642b12c975f8b0bb0a6104",
+    "url": "/counter/static/media/back.ba55fe4d.jpg"
+  }
+]);
